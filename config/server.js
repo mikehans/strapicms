18364b08ab3d66fdc411056714322035d8cb03bb
Mike Hansford <mjhansford@gmail.com>
2021-11-10 14:37:28 +1100

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e32618241b6e41bc423e945f7ff8699d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e2403d38-5cdb-44ff-9bb3-9f88c0fd9463'
+};
